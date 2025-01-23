3f1593c87f12d2e0b75f8735cb2329ce756e246f
melisacar <melisa.acar@ipa.istanbul>
2025-01-23 14:08:34 +0300

    feat: code for user registration api

--- main.go
+++ main.go
@@ -2,25 +2,69 @@ package main
 
 import (
 	"net/http"
+	"regexp"
+
+	// https://pkg.go.dev/regexp
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	// https://pkg.go.dev/github.com/labstack/echo/v4/middleware
 )
+
+// Defining the User Struct
+type User struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
 
 func main() {
 
-	// Create Echo instance
+	// Echo instance
 	e := echo.New()
 
-	// Root endpoint
-	// We define a GET endpoint with a dynamic URL parameter.
-	// This determines the function that will be run when someone sends a GET request to http://localhost:1212/<name>.
-	e.GET("/:name", func(c echo.Context) error {
-		name := c.Param("name")                             // Extract the value from the URL parameter.
-		return c.String(http.StatusOK, "Hello!, "+name+"!") // Send a response with the extracted name.
+	// Middleware to log requests
+	e.Use(middleware.Logger())
 
+	// Register endpoint
+	e.POST("/register", func(c echo.Context) error {
+
+		// Initialize a User struct to bind incoming data
+		var user User // Creates a variable user of type User
+
+		// Bind JSON body to the struct
+		if err := c.Bind(&user); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"error": "Invalid request",
+			})
+		}
+
+		// Validate email format
+		if !isValidEmail(user.Email) {
+			return c.JSON(http.StatusUnprocessableEntity, map[string]string{
+				"error": "Invalid email format",
+			})
+
+		}
+
+		// Return success response
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "User registered successfully",
+			"user": map[string]string{
+				"name":  user.Name,
+				"email": user.Email,
+			},
+		})
 	})
 
 	// Start the server and listen on port 1212
 	e.Logger.Fatal(e.Start(":1212"))
+}
 
+// Email validation function
+func isValidEmail(email string) bool {
+	// Basic email regex
+	regex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(regex)
+	return re.MatchString(email)
 }
